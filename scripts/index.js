487c67e333660758ceb183e6e28e226beb3ab41f
Matvey Kham <mvham@mail.ru>
2022-01-17 19:33:27 +0800

    fix_1: исправление 12 ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,28 @@ let popup = document.querySelector('.popup');
 let buttonEdit = document.querySelector('.profile__edit-button');
 let buttonEditClose = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__form-name');
-let jobInput = document.querySelector('.popup__form-position');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_position');
 let profileName = document.querySelector('.profile__title');
 let profilePos = document.querySelector('.profile__position');
 
-buttonEdit.addEventListener('click', function() {
-    popup.classList.add('popup__opened');
+function openPopup() {
+    popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profilePos.textContent;
-});
+}
 
-buttonEditClose.addEventListener('click', function() {
-    popup.classList.remove('popup__opened');
-});
+function clsPopup() {
+    popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profilePos.textContent = jobInput.value;
     profileName.textContent = nameInput.value;
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
 
+buttonEdit.addEventListener('click', openPopup);
+buttonEditClose.addEventListener('click', clsPopup);
 formElement.addEventListener('submit', formSubmitHandler);
